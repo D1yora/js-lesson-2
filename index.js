c9606655da4d947b1f3db276fd93dc72872058ed
D1yora <dioora2o3@gmail.com>
2023-01-26 19:58:41 +0500

    homework & sths

---
+++ index.js
@@ -0,0 +1,93 @@
+// homework
+
+let name = "Ilya";
+
+console.log(alert( `hello ${1}` ));
+alert( `hello ${"name"}` );
+alert( `hello ${name}`);
+
+
+
+// type conversion
+
+// let a = 1;
+// let b = "1";
+
+// console.log(+b);
+
+
+// =======
+
+// let a = "10";
+// let b = "10.3"
+
+// console.log(Number(a));
+// console.log(Number.parseInt(b));
+// console.log(Number.parseFloat(b));
+
+
+// ========
+// true = !0, !null, !undefined
+// console.log(false == 0);
+// console.log(true == 1);
+// console.log(false == null);
+// console.log(false == undefined);
+// console.log(undefined == null);
+
+// console.log(true == "1");
+// console.log(true == "a");
+// console.log(true == "2");
+// console.log(true == "0");
+
+// ==========
+
+// let a = 1;
+// let b = "10";
+// console.log(a * +b);
+
+// ========= --, ++
+// let a = 1;
+// a++
+// // a = a + 1
+// console.log(a++);
+// console.log(++a);
+
+// abbrivations
+// let a = 555;
+
+// a = a + 555;
+// a += 1000;
+// console.log(a);
+
+// ===========
+// Comparison
+// let a = 10;
+// let b = 11;
+// let c = 11;
+
+
+// = variable
+// == taqqoslash
+// === data type ni taqqoslash(Qat'iy tenglik)
+
+// console.log(c === b);
+// console.log(a < b);
+// console.log(c > b);
+// console.log(c <= b); // not c => b
+
+// ===========
+// Logical  operations
+// OR
+// let ceo = true;
+// let admin = true;
+
+// console.log(admin === true || ceo === true);
+// console.log(admin || ceo);
+// console.log(false || true);
+
+// AND &&
+
+// let id = true;
+// let face = false;
+
+// console.log(id && face);
